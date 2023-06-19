5d929232a22cf52b51f3868265e4d9a56803ae32
Suphakit P <me@suphakit.net>
2023-06-19 21:35:25 +0700

    fix: 1 digit show up

--- ios-widget-interface/src/js/time.js
+++ ios-widget-interface/src/js/time.js
@@ -3,18 +3,18 @@ const timeElement = document.querySelector('#time')
 const timeUpdate = setInterval(function () {
   const currentTime = new Date().getTime() - 1686762000000
 
-  let hour = Math.floor((currentTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  let hour = (Math.floor((currentTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).toString()
   let min = Math.floor((currentTime % (1000 * 60 * 60)) / (1000 * 60)).toString()
   let sec = Math.floor((currentTime % (1000 * 60)) / 1000)
 
   const timeCounting = `<h1>
-  ${hour}
+  ${(hour.length < 2) ? `0${hour}` : hour}
 </h1>
 <h1 style="transform: rotate(90deg);">
   ${((sec % 2) != 0) ? ':' : '&nbsp;'}
 </h1>
 <h1>
-  ${(min < 2) ? `0${min}` : min}
+  ${(min.length < 2) ? `0${min}` : min}
 </h1>`
 
   timeElement.innerHTML = timeCounting
